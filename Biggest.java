937caee592dcb8ed2e015d258acaf5dba7b2dc40
01FE22BCA126 <156191525+01FE22BCA126@users.noreply.github.com>
2024-02-07 17:57:11 +0530

    Update to asceding order

--- Biggest.java
+++ Biggest.java
@@ -1,17 +1,19 @@
-public class Biggest {
-
-    private static int findBiggest(int a, int b, int c) {
-        if (a >= b && a >= c) {
-            return a;
-        } else if (b >= a && b >= c) {
-            return b;
-        } else {
-            return c;
-        }
-    }
-
-    public static void main(String[] args) {
-        int result = findBiggest(10, 20, 30);
-        System.out.println("The biggest number is: " + result);
-    }
-}
+public class Biggest{    
+    public static void main(String[] args) {       
+        int [] num = new int [] {20,30,40,33};     
+        int temp = 0;    
+        for (int i = 0; i < num.length; i++) {     
+            for (int j = i+1; j < num.length; j++) {     
+               if(num[i] > num[j]) {    
+                   temp = num[i];    
+                  num[i] = num[j];    
+                   num[j] = temp;    
+               }     
+            }     
+        }    
+        System.out.println("Elements in ascending order: ");    
+        for (int i = 0; i < num.length; i++) {     
+            System.out.print(num[i] + " ");    
+        }    
+    }    
+}   
